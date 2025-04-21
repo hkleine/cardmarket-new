ed0cfe71332122fa4b8084e9e486eaa47289d508
hkleine <hendrik-kleine@gmx.de>
2025-04-21 11:19:03 +0200

    awdawd

--- app/(main)/onboarding/page.tsx
+++ app/(main)/onboarding/page.tsx
@@ -1,20 +1,9 @@
 "use client";
 
 import { CheckCircle2 } from "lucide-react";
-import { useSearchParams } from "next/navigation";
 
 export default function OnboardingPage() {
-  const searchParams = useSearchParams();
-  const merchantId = searchParams.get("merchantId");
-  const merchantIdInPayPal = searchParams.get("merchantIdInPayPal");
-  const permissionsGranted = searchParams.get("permissionsGranted");
-  const consentStatus = searchParams.get("consentStatus");
-  console.log(merchantId, merchantIdInPayPal);
-  return (
-    <div>
-      {permissionsGranted && consentStatus ? <SuccessMessage /> : <div></div>}
-    </div>
-  );
+  return <SuccessMessage />;
 }
 
 function SuccessMessage() {
